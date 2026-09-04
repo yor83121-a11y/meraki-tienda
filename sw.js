0cf4d3c835b592b8b854f00477e7c882fa96faeb
yor83121-a11y <yor83121@gmail.com>
2026-09-03 23:20:24 -0600

    Add files via upload

---
+++ sw.js
@@ -0,0 +1,1 @@
+const CACHE='meraki-v1';self.addEventListener('install',e=>e.waitUntil(caches.open(CACHE).then(c=>c.addAll(['./','./index.html','./manifest.webmanifest','./icon.svg']))));self.addEventListener('fetch',e=>{if(e.request.method==='GET'&&new URL(e.request.url).origin===location.origin)e.respondWith(caches.match(e.request).then(r=>r||fetch(e.request)))});
